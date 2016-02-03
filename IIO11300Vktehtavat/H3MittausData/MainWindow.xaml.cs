db51eb54f955b354e743588b6994f3417e1f5a69
vuoale <H8346@student.jamk.fi>
2016-02-03 09:49:46 +0200

    Toteutus olio kokoelmalla

--- IIO11300Vktehtavat/H3MittausData/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/H3MittausData/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace H3MittausData
     /// </summary>
     public partial class MainWindow : Window
     {
+        //luodaan kokoelma mittaus-olioille
+        List<MittausData> mitatut;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,13 +32,24 @@ namespace H3MittausData
         {
             //omat ikkunaan liittyvät alustukset
             txtToday.Text = DateTime.Today.ToShortDateString();
+            mitatut = new List<MittausData>();
         }
 
         private void btnSaveData_Click(object sender, RoutedEventArgs e)
         {
             //luodaan uusi mittausdata olio ja näytetään se käyttäjälle
             MittausData md = new MittausData(txtClock.Text, txtData.Text);
-            lbData.Items.Add(md);
+            //lbData.Items.Add(md); //alkuperäinen tapa
+            //lisätään mittaus-olio kokoelmaan
+            mitatut.Add(md);
+            ApplyChanges();
+        }
+
+        private void ApplyChanges()
+        {
+            //päivitetään UI vastaamaan kokoelman tietoja
+            lbData.ItemsSource = null;
+            lbData.ItemsSource = mitatut;
         }
     }
 }
