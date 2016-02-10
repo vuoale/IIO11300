7eb9164a468edd1ff0330963da87a7c59c4af6ad
vuoale <H8346@student.jamk.fi>
2016-02-10 11:06:29 +0200

    Add H4WpfXML

---
+++ IIO11300Vktehtavat/H4WpfXML/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace H4WpfXML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        XElement xe;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetXML_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //ladataan xml-tiedosto ja asetetaan se Datagridin "data context":ksi
+                xe = XElement.Load(GetFileName());
+                dgData.DataContext = xe.Elements("tyontekija");
+                //lasketaan työntekijöitten määrä ja palkkasumma ja näytetään ne käyttäjälle
+                int lkm = 0;
+                lkm = xe.Elements().Count();
+                tbMessage.Text = string.Format("Akun tehtaalla on kaikkiaan {0} työntekijää, joista vakituisia {2} palkat yhteensä {1}", lkm, CalculateSalarySum(), CountWorkers("vakituinen"));
+            }
+            catch (Exception ex)
+            {              
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string GetFileName()
+        {
+            //älä kovakoodaa muuttuvia asioita koodiin
+            //return "d:\\Työntekijät.xml";
+
+            //parempi tapa on sijoittaa ne App.Config-konfigurointitiedostoon
+            return H4WpfXML.Properties.Settings.Default.XMLTiedosto;
+        }
+
+        private decimal CalculateSalarySum()
+        {
+            decimal result = 0;
+            //haetaan työntekijöitten palkat xml:stä (XElement-olioon) LINQ-kyselyllä
+            var palkat = from ele in xe.Elements()
+                         select ele.Element("palkka");
+            foreach (var item in palkat)
+            {
+                result += decimal.Parse(item.Value);
+            }
+            return result;
+        }
+        private int CountWorkers(string tyosuhde)
+        {
+            //lasketaan annetun työsuhteen mukaiset työntekijät LINQ-kyselyllä
+            var tyontekijat = from ele in xe.Elements()
+                              where ele.Element("tyosuhde").Value == tyosuhde
+                              select ele.Element("etunimi");
+            //palautetaan lukumäärä
+            return tyontekijat.Count();
+        }
+    }
+}
