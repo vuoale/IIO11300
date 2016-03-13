f058ebf6a4fcd1ce5363604baaf188462c625bbf
vuoale <H8346@student.jamk.fi>
2016-03-13 17:21:00 +0200

    Tehtävä 8 ratkaistu kokonaan

---
+++ IIO11300Vktehtavat/Tehtava8/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;  //sisältää ADO;n perusluokat
+using System.Data.SqlClient; //SQL Server spesifiset luokat
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetData();
+        }
+        private void GetData()
+        {
+            try
+            {
+                using (SqlConnection conn =
+                  new SqlConnection(Tehtava8.Properties.Settings.Default.Tietokanta))
+                {
+                    //yhteys
+                    //dataadapter
+                    string sql = "SELECT firstname, lastname, address, city FROM vCustomers";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable("Lasnaolot");
+                    da.Fill(dt);
+                    //sidotaan datatable UI-kontrolliin
+                    listBox.DataContext = dt;
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnHaeAsiakkaat_Click(object sender, RoutedEventArgs e)
+        {
+            GetData();
+        }
+    }
+}
