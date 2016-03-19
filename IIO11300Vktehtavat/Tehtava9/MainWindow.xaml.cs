5249a045b0613d5bafdd2b8d89180d6e38456f70
vuoale <H8346@student.jamk.fi>
2016-03-19 18:32:26 +0200

    Tehtävä 9 ratkaistu kokonaan

---
+++ IIO11300Vktehtavat/Tehtava9/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHae_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                dataGrid.DataContext = Tietokanta.GetData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUusi_Click(object sender, RoutedEventArgs e)
+        {
+            stackPanel.Visibility = Visibility.Visible;
+        }
+
+        private void btnLuo_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtEnimi.Text) && !string.IsNullOrWhiteSpace(txtSnimi.Text) && !string.IsNullOrWhiteSpace(txtOsoite.Text) && !string.IsNullOrWhiteSpace(txtPostinro.Text) && !string.IsNullOrWhiteSpace(txtKaupunki.Text))
+            {
+                try
+                {
+                    Tietokanta.InsertData(txtEnimi.Text, txtSnimi.Text, txtOsoite.Text, txtPostinro.Text, txtKaupunki.Text);
+                    MessageBox.Show("Lisäys onnistui");
+                    dataGrid.DataContext = Tietokanta.GetData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("Täytä kaikki kentät");
+        }
+
+        private void btnPoista_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedIndex > 0)
+            {
+                DataRowView drv = (DataRowView)dataGrid.SelectedItem;
+                string sNimi = drv.Row[2].ToString();
+                string eNimi = drv.Row[1].ToString();
+                string id = drv.Row[0].ToString();
+
+                MessageBoxResult mbr = MessageBox.Show("Haluatko varmasti poistaa asiakkaan " + eNimi + " " + sNimi + "?", "Poista asiakas", MessageBoxButton.YesNo);
+                if (mbr == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Tietokanta.DeleteData(id);
+                        MessageBox.Show("Poisto onnistui");
+                        dataGrid.DataContext = Tietokanta.GetData();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava9/Tietokanta.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava9
+{
+    public class Tietokanta
+    {
+        public static DataTable GetData()
+        {
+            try
+            {
+                using (SqlConnection conn =
+                  new SqlConnection(Tehtava9.Properties.Settings.Default.Tietokanta))
+                {
+                    string sql = "SELECT * FROM customer";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt;
+                    dt = new DataTable("dt");
+                    da.Fill(dt);
+                    conn.Close();
+                    return dt;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void InsertData(string firstname, string lastname, string address, string zip, string city)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Tehtava9.Properties.Settings.Default.Tietokanta);
+                conn.Open();
+                string sql = string.Format("INSERT INTO customer (firstname, lastname, address, zip, city) VALUES ('{0}','{1}','{2}','{3}','{4}')", firstname, lastname, address, zip, city);
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void DeleteData(string id)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Tehtava9.Properties.Settings.Default.Tietokanta);
+                conn.Open();
+                string sql = string.Format("DELETE FROM customer WHERE id = '{0}'", id);
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
